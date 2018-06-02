8b14c4fe50bc36986ef0b73baad9d281978e8de0
philipjc <philipcox83@gmail.com>
2018-06-02 21:10:57 +0100

    AWS setup.

--- website/settings/production.py
+++ website/settings/production.py
@@ -7,6 +7,23 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['anth-site.herokuapp.com']
 
+AWS_ACCESS_KEY = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'anthsite'
+
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400'
+}
+
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https:''%s/%s' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storage.backends.s3boto3.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3BotoStorage'
+
+
 try:
     from .local import *
 except ImportError:
