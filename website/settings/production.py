69f874445bbffefd099315f7c16a0b1bb5d00bea
philipjc <philipcox83@gmail.com>
2018-06-02 21:24:02 +0100

    AWS setup. s3 name error fix

--- website/settings/production.py
+++ website/settings/production.py
@@ -20,8 +20,8 @@ AWS_S3_OBJECT_PARAMETERS = {
 AWS_LOCATION = 'static'
 
 STATIC_URL = 'https:''%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storage.backends.s3boto3.S3BotoStorage'
-DEFAULT_FILE_STORAGE = 'storage.backends.s3boto3.S3BotoStorage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 
 try:
