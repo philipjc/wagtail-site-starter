ab2ef930bc8ad7175d46eb082442d68014edfb0e
philipjc <philipcox83@gmail.com>
2018-09-11 15:59:53 +0100

    saving new subscriber with validation.

--- apps/newsletter/urls.py
+++ apps/newsletter/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import url
 from django.views.generic import TemplateView
 
 
-from .views import signup
+from .views import news_signup
 
 app_name = 'newsletter'
 
 urlpatterns = [
 
-    url(r'^$', signup, name='signup'),
+    url(r'^$', news_signup, name='signup'),
     url(r'^thanks/$', TemplateView.as_view(template_name="newsletter/thankyou.html"), name='thanks'),
 
 ]
--- apps/newsletter/views.py
+++ apps/newsletter/views.py
@@ -1,19 +1,36 @@
 from django.shortcuts import redirect
 from django.http import Http404
 from django.urls import reverse_lazy
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
 
-# import sendgrid
-# from sendgrid.helpers.mail import *
-# from decouple import config
+from .models import NewsLetterSubscriber
 
 
-def signup(request):
-    name = request.POST.get('enquiry_name', '')
-    form_email = request.POST.get('enquiry_email', '')
+def news_signup(request):
 
-    if form_email == '':
+    validator = EmailValidator()
+    form_email = request.POST.get('newsletter_email', '')
+
+    try:
+        validator(form_email)
+        valid_email = True
+
+    except ValidationError:
+        valid_email = False
+
+    if not valid_email:
+        print("invalid email")
         raise Http404
 
     else:
-        return redirect(reverse_lazy('enquiry:thanks'))
 
+        try:
+            subscriber = NewsLetterSubscriber(email=form_email)
+            subscriber.save()
+
+            return redirect(reverse_lazy('newsletter:thanks'))
+
+        except Exception as error:
+            print("Failed to save new subscriber", error)
+            raise Http404
